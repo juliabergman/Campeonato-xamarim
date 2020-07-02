ec98f735724c35c02f79ee9ea306a926bfa2435c
MattSouuza <matheus_ss02@hotmail.com>
2020-07-02 14:16:44 -0300

    Readding the project

---
+++ Campeonato/Campeonato/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Campeonato.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Campeonato
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
